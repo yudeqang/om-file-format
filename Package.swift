e20148fbb3d556e6d13fce65379f8a1fcbf6ab34
patrick-zippenfenig <patrick@zippenfenig.de>
2024-12-25 10:18:55 +0100

    rename targets

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -26,22 +25,21 @@ let package = Package(
         .macOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OmFileFormat",
             targets: ["OmFileFormatSwift"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "OmFileFormatSwift",
             dependencies: ["OmFileFormatC"],
+            path: "Swift/OmFileFormatSwift",
             cSettings: cFlagsPFor2D,
             swiftSettings: swiftFlags
         ),
         .target(
             name: "OmFileFormatC",
+            path: "c",
             cSettings: cFlagsPFor,
             swiftSettings: swiftFlags
         ),
